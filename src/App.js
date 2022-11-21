b1572d5d8a6f0ee2afe49da8f78ebf64d240d5da
Swadip <roysoumo1607@gmail.com>
2022-11-21 18:22:58 +0600

    practice readux fundamental

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {actions} from './store/store';
 
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  const incrementHandler = () => {
+    dispatch(actions.increment())
+  } 
+  const decrementHandler = () => {
+    dispatch(actions.decrement())
+  }
+  const addBy = () => {
+    dispatch(actions.addBy(10))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginLeft: '20px', color: 'red'}}>
+      <h1>Counter App</h1>
+      <h3> {counter} </h3>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler} style={{marginLeft: '20px'}}>Decrement</button>
+      <button onClick={addBy} style={{marginLeft: '20px'}}>Add by 10</button>
     </div>
   );
 }
